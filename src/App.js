e259505e87b3ae14f8d40c05b80e77a814c1fc6e
Nidhi pal <nidhipalmms@gmail.com>
2021-12-16 15:32:00 +0530

    Cors chrome extension added

--- src/App.js
+++ src/App.js
@@ -2,42 +2,52 @@ import React, { useEffect } from 'react'
 import XMLParser from 'react-xml-parser';
 let axios = require("axios");
 
-function App() {
-  const baseURL = "https://api.morningstar.com/service/mf/DailyReturnIndex/isin/LU0056508442?accesscode=w9u021kzu8m1res1z5vzt0z4j5f39fur&startdate=2020-12-03&enddate=2021-12-14&frequency=D";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
 
-  const [data, setData] = React.useState([]);
+  async componentDidMount() {
+    const res = await axios.get("https://api.morningstar.com/service/mf/DailyReturnIndex/isin/LU0056508442?accesscode=w9u021kzu8m1res1z5vzt0z4j5f39fur&startdate=2020-12-03&enddate=2021-12-14&frequency=D");
+    let resData = res.data;
+    var xmlData = new XMLParser().parseFromString(resData);
+    this.setState({
+      data: xmlData.children[0].children[0].children
+    })
+    console.log(this.state.data);
+  }
 
-  useEffect(() => {
-    axios.get(baseURL)
-      .then(res => res.text())
-      .then(XMLData => {
-        var jsonDataFromXml = new XMLParser().parseFromString(XMLData);
-        setData(jsonDataFromXml.getElementsByTagName('r'));
-      })
-      .catch(err => console.log(err));
 
-  }, [])
+  render() {
 
-  return (
-    <div>
-      <h2>Fetched API data</h2>
-      <table>
-        <tr>
-          <th>Value</th>
-          <th>Date</th>
-        </tr>
-        {
-          {data}.map((item) =>
+    return (
+      <div>
+        <h2 style={{marginLeft: "500px"}}>Fetched API data</h2>
+        <table style={{marginLeft: "500px"}}>
+          <thead>
             <tr>
-              <td>{item.v}</td>
-              <td>{item.d}</td>
+              <th style={{width:'5rem'}}>Value</th>
+              <th>Date</th>
             </tr>
-          )
-        }
+          </thead>
+          <tbody>
+            {
+              this.state.data.map((item) => (
+                <tr>
+                  <td>{item.attributes.v}</td>
+                  <td>{item.attributes.d}</td>
+                </tr>
 
-      </table>
-    </div>
-  )
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App
